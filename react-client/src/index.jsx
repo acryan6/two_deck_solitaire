560f47787ffd54131c2c38f771c5de13a8f5e830
Andy Cryan <acryan6@gatech.edu>
2020-06-06 10:38:51 -0600

    Separate into piles, start CSS

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -28,14 +28,34 @@ class App extends React.Component {
   componentDidMount() {
     fetch("/api/cards")
       .then((res) => res.json())
-      .then((data) => this.setState({ cards: data }))
+      .then((data) => {
+        this.setState({
+          cards: data,
+          A: data.slice(0, 3),
+          1: data.slice(3, 6),
+          2: data.slice(6, 9),
+          3: data.slice(12, 15),
+          4: data.slice(15, 18),
+          5: data.slice(18, 21),
+          6: data.slice(21, 24),
+          7: data.slice(24, 27),
+          8: data.slice(27, 30),
+          9: data.slice(30, 33),
+          10: data.slice(33, 36),
+          J: data.slice(36, 39),
+          Q: data.slice(39, 42),
+          K: data.slice(42, 45),
+          drawStack: data.slice(45, 52),
+        });
+      })
       .catch((err) => console.log(err));
   }
 
   render() {
     return (
       <div>
-        <h1>Cards List</h1>
+        <h1>Mini-Moo</h1>
+        <h3>A Form of Devil's Solitaire(?)</h3>
         <Board cards={this.state.cards} />
       </div>
     );
