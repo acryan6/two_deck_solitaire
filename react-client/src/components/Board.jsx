60dd35bf16ffeef4df1bb3d1f656fc5b11bb0fe1
Andy Cryan <acryan6@gatech.edu>
2020-07-12 14:17:24 -0600

    Pass initial state to board piles

--- react-client/src/components/Board.jsx
+++ react-client/src/components/Board.jsx
@@ -2,13 +2,54 @@ import React from "react";
 import { useSelector } from "react-redux";
 import Card from "./Card.jsx";
 import Drawstack from "./Card_back.png";
+var pile_A,
+  pile_2,
+  pile_3,
+  pile_4,
+  pile_5,
+  pile_6,
+  pile_7,
+  pile_8,
+  pile_9,
+  pile_0,
+  pile_J,
+  pile_Q,
+  pile_K;
 
 const Board = (props) => {
-  let pile_A, pile_2;
-  [pile_A, pile_2] = useSelector((state) => {
-    return [state.get("game").get("pile_A"), state.get("game").get("pile_2")];
+  [
+    pile_A,
+    pile_2,
+    pile_3,
+    pile_4,
+    pile_5,
+    pile_6,
+    pile_7,
+    pile_8,
+    pile_9,
+    pile_0,
+    pile_J,
+    pile_Q,
+    pile_K,
+  ] = useSelector((state) => {
+    let game = state.get("game");
+    return [
+      game.get("pile_A"),
+      game.get("pile_2"),
+      game.get("pile_3"),
+      game.get("pile_4"),
+      game.get("pile_5"),
+      game.get("pile_6"),
+      game.get("pile_7"),
+      game.get("pile_8"),
+      game.get("pile_9"),
+      game.get("pile_0"),
+      game.get("pile_J"),
+      game.get("pile_Q"),
+      game.get("pile_K"),
+    ];
   });
-  console.log(pile_A, pile_2);
+
   return (
     <div className="container-fluid">
       <div className="row board-row">
