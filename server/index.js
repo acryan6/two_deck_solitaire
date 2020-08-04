2b6e0d47454450a18ae7936f70c7d9ef892898ca
Andy Cryan <acryan6@gatech.edu>
2020-08-04 17:08:47 -0600

    Simplify backend for heroku

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 const request = require("request-promise");
-var db = require("../database-mongo");
+// var db = require("../database-mongo");
 
 var app = express();
 app.use(bodyParser.json());
@@ -22,23 +22,23 @@ app.get("/api/cards", function (req, res) {
     .catch((err) => console.log(err));
 });
 
-app.post("/api/cards", async (req, res) => {
-  request("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-    .then((res) => JSON.parse(res))
-    .then((deck) => {
-      return request(
-        `https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=52`
-      );
-    })
-    .then((res) => JSON.parse(res))
-    .then((deck) => {
-      db.saveCards(deck.cards);
-    })
-    .then(() => {
-      res.send("saved!");
-    })
-    .catch((err) => console.log(err));
-});
+// app.post("/api/cards", async (req, res) => {
+//   request("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+//     .then((res) => JSON.parse(res))
+//     .then((deck) => {
+//       return request(
+//         `https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=52`
+//       );
+//     })
+//     .then((res) => JSON.parse(res))
+//     .then((deck) => {
+//       db.saveCards(deck.cards);
+//     })
+//     .then(() => {
+//       res.send("saved!");
+//     })
+//     .catch((err) => console.log(err));
+// });
 
 app.listen(3000, function () {
   console.log("listening on port 3000!");
